673df2158760a8a05333fd85575bce331a5215ba
Pranto Deb <rajprit0m@outlook.com>
2024-01-29 18:30:56 +0600

    1st task OK

---
+++ task_1.js
@@ -0,0 +1,14 @@
+// Declare an array
+// Declare an array with 5 elements containing fruits
+// console log the 3rd index element
+// change the value of the 2nd index element to jambura
+// console log the final array
+
+let fruits = ["🍉", "🍇", "🍈", "🍏", "🍎", "🥭", "🍍", "🍒"];
+console.log(fruits);
+
+console.log("3rd index element is : ", fruits[3]);
+
+fruits[2] = "🥝";
+
+console.log(fruits);
