45a04834ee4cead6dcbde33bafe04ce41fd6fbc1
Pranto Deb <rajprit0m@outlook.com>
2024-01-29 18:38:39 +0600

    2nd task OK

---
+++ task_2.js
@@ -0,0 +1,19 @@
+// Add or remove elements
+// Declare an array of 3 tourist destinations
+// Add a new tourist destination to your tourist array
+// Add two more to your array
+// Remove the last tourist destination you have added
+// display the final array as output
+
+let touristPlace = ["Bandarban", "CoxBazar", "Rangamati"];
+console.log(touristPlace);
+
+touristPlace.push('Rajasthan');
+console.log(touristPlace);
+
+touristPlace.push("Kasmir", "Nepal");
+console.log(touristPlace);
+
+touristPlace.pop();
+console.log(touristPlace);
+
