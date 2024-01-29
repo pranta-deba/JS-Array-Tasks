e602d4b5d56610cbe3c1fc3e73024626d0fa03c0
Pranto Deb <rajprit0m@outlook.com>
2024-01-29 18:44:45 +0600

    3rd task OK

---
+++ task_3.js
@@ -0,0 +1,16 @@
+// Checking Array Membership with ‘includes’
+// Instructions:
+
+// Create an array of books containing different book.
+
+// Use the includes method to check if the array contains a javascript book.
+
+// Print a message to the console indicating whether the element is present in the array or not.
+
+let library = ["JS", "PHP", "HTML", "CSS", "PYTHON", "REACT"];
+
+if (library.includes("JS")) {
+    console.log("JS Book is available");
+}else{
+    console.log("Sorry, JS Book is not available");
+}
